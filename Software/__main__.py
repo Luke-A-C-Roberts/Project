ab0f2207a39a2677b4e587048dc95c36e7323bd1
Luke-A-C-Roberts <101272187+Luke-A-C-Roberts@users.noreply.github.com>
2024-03-17 22:57:05 +0000

    Update __main__.py

--- Software/__main__.py
+++ Software/__main__.py
@@ -2,14 +2,12 @@ import tensorflow as tf
 
 from alexnet import build_alex_net
 from resnet import build_resnet
-from datasets import training_df
+from datasets import zenodo_ids, training_df, training_data
 
-from datasets import *
+from functools import partial
 
 def main():
-    df = training_df()
-    print(df)
-
+    data = training_data(partial(training_df, zenodo_ids))
 
 if __name__ == "__main__":
     main()
