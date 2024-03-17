5beb8f8a4b08abc2433c2bcc5a698b665419154c
Luke-A-C-Roberts <101272187+Luke-A-C-Roberts@users.noreply.github.com>
2024-03-17 22:28:16 +0000

    Update datasets.py

--- Software/datasets.py
+++ Software/datasets.py
@@ -1,6 +1,9 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 # For datasets see galaxy zoo 2:
-#    labels: https://data.galaxyzoo.org/#section-7 and images: https://zenodo.org/records/3565489#.Y3vFKS-l0eY
+#    labels: https://data.galaxyzoo.org/#section-7
+#    images: https://zenodo.org/records/3565489#.Y3vFKS-l0eY,
+#            https://portal.nersc.gov/project/dasrepo/self-supervised-learning-sdss/dataset.html
+#
 # The script assumes that windows subsystem for linux is available on your platform.
 # gz2_filename_mapping.csv, gz2_hart16.csv and images_gz2 must all be found in your ...\Downloads folder.
 # NOTE: windows extract fails to extract gz2_hart16.csv correctly so use a different program like WinRAR
@@ -20,34 +23,53 @@ from pandas import DataFrame as pd_DataFrame
 
 from os import listdir
 from re import match
+from typing import Callable
 
 # Might need to change these
-FOLDER_PATH  : str = "/mnt/c/Users/Computing/Downloads/"
-MAPPING_FILE : str = FOLDER_PATH + "gz2_filename_mapping.csv"
-DATASET_FILE : str = FOLDER_PATH + "gz2_hart16.csv"
-IMAGES_FOLDER: str = FOLDER_PATH + "images_gz2/images/"
+DOWNLOADS_PATH      : str = "/mnt/c/Users/Computing/Downloads/"
+MAPPING_FILE        : str = DOWNLOADS_PATH + "gz2_filename_mapping.csv"
+DATASET_FILE        : str = DOWNLOADS_PATH + "gz2_hart16.csv"
+ZENODO_IMAGES_FOLDER: str = DOWNLOADS_PATH + "images_gz2/images/"
 
-DATASET_COLS      : list[str] = ["dr7objid", "sample", "gz2_class"]
-FILENAME_DROP_COLS: list[str] = ["asset_id", "id", "sample"]
-DF_DROP_COLS      : list[str] = ["dr7objid", "objid"]
-
+DATASET_COLS       : list[str] = ["dr7objid", "sample", "gz2_class"]
+FILENAME_DROP_COLS : list[str] = ["asset_id", "id", "sample"]
+DF_DROP_COLS       : list[str] = ["dr7objid", "objid"]
 CLASSIFICATION_STEM: list[str] = ["Er", "Ei", "Ec", "Ser", "Seb", "Sen", "Sa", "Sb", "Sc", "Sd", "SBa", "SBb", "SBc", "SBd"]
+CLASSIFICATION_INT : list[int] = [0, 0, 0, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3]
+
+spark: SparkSession = SparkSession.builder.getOrCreate()
 
 # https://sparkbyexamples.com/pyspark/pyspark-udf-user-defined-function/
 @udf(returnType=StringType())
 def remove_jpg_extention(name: str) -> str:
     return name.removesuffix(".jpg")
 
+
 @udf(returnType=IntegerType())
-def classification(gz2_class: str) -> int:
+def classification(gz2_class: str) -> str:
     for stem in CLASSIFICATION_STEM:
         m = match(r"^" + stem, gz2_class)
         if m:
-            return stem
-    return "None"
+            return CLASSIFICATION_INT[CLASSIFICATION_STEM.index(stem)]
+    return -1
 
-def training_df() -> pd_DataFrame:
-    spark: SparkSession = SparkSession.builder.getOrCreate()
+
+# https://zenodo.org/records/3565489#.Y3vFKS-l0eY
+def zenodo_ids() -> DataFrame:
+    """
+    zenodo_id gets the object IDs from the extentions in /mnt/c/Users/Computing/Downloads/images_gz2/images/ once they have been downloaded from https://zenodo.org/records/3565489#.Y3vFKS-l0eY. Make sure both the images_gz2 and gz2_filename_mapping.csv are downloaded. 
+    """
+    return (
+        spark
+        .createDataFrame(listdir(ZENODO_IMAGES_FOLDER), schema=StringType())
+        .withColumn("id", remove_jpg_extention(col("value")))
+    )
+
+
+def training_df(obj_ids: Callable[[None], DataFrame]) -> pd_DataFrame:
+    """
+    training df, takes an fuction that gives a dataframe of IDs and produces a list of existing image files names and classification numbers.
+    """
     mapping_names: DataFrame = spark.read.csv(path=MAPPING_FILE, header=True, inferSchema=True)
 
     data_set: DataFrame = (
@@ -57,26 +79,23 @@ def training_df() -> pd_DataFrame:
         .select(*DATASET_COLS)
     )
 
-    file_names: DataFrame = (
-        spark
-        .createDataFrame(listdir(IMAGES_FOLDER), schema=StringType())
-        .withColumn("id", remove_jpg_extention(col("value")))
-    )
-    file_names = (
-        file_names
-        .join(mapping_names, file_names["id"] == mapping_names["asset_id"], how="inner")
+    obj_ids: DataFrame = obj_ids()
+    obj_ids = (
+        obj_ids()
+        .join(mapping_names, obj_ids["id"] == mapping_names["asset_id"], how="inner")
         .drop(*FILENAME_DROP_COLS)
     )
 
-    df = file_names.join(data_set, file_names["objid"] == data_set["dr7objid"], how="inner")
+    df = obj_ids.join(data_set, obj_ids["objid"] == data_set["dr7objid"], how="inner")
     df = (
         df
         .sort(df["value"])
         .drop(*DF_DROP_COLS)
         .withColumn("classification", classification(col("gz2_class")))
+        .filter(col("classification") != -1)
     )
 
     return df.toPandas()
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
-training_df().to_csv("/mnt/c/Users/Computing/Desktop/Project/Software/Final/result.csv")
+# training_df(zenodo_ids).to_csv("/mnt/c/Users/Computing/Desktop/Project/Software/Final/result.csv")
